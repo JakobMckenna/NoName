4b578cf4e9790efef948c6811544e9353cf6ec1e
Tehilah <tkangamba@hotmail.com>
2023-10-01 14:38:54 -0500

    added some meaningful comments to user_controller

--- backend/controller/user_controller.ts
+++ backend/controller/user_controller.ts
@@ -2,23 +2,25 @@ import { Request, Response } from 'express';
 import UserService from '../services/user_service';
 
 const UserController = {
-    getUser:(req:Request,res:Response)=>{
+    getUser: (req: Request, res: Response) => {
         const userData = UserService.getUser()
         res.send(userData);
     },
 
-    createUser:async (req:Request,res:Response)=>{
+    createUser: async (req: Request, res: Response) => {
         const userBody = req.body;
-       // console.log(userBody)
-        const name:string = userBody.name;
-        const email:string = userBody.email;
-        const password:string = userBody.password;
-       // console.log(password)
-        const user = await UserService.createUser(email , name, password)
-        if(user)
-            res.status(201).json({"user":user})
-        else{
-            res.status(400).json({"user":user})
+        const name: string = userBody.name;
+        const email: string = userBody.email;
+        const password: string = userBody.password;
+      
+        const user = await UserService.createUser(email, name, password);
+        if (user) {
+            // returns user object if successfully create
+            res.status(201).json({ "user": user })
+        }
+        else {
+            // returns null user object if user exists already or failed for some other reason
+            res.status(400).json({ "user": user })
         }
     }
 }
