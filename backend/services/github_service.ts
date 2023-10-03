558ed5bb63c5286a92532b876bf1c768227f89c5
Tehilah <tkangamba@hotmail.com>
2023-10-03 17:17:18 -0500

    Finished get comits function in github service

--- backend/services/github_service.ts
+++ backend/services/github_service.ts
@@ -1,13 +1,31 @@
 import axios from 'axios';
 
 
-const apiKey = process.env.GIT_TOKEN;
+const GIT_TOKEN = process.env.GIT_TOKEN;
+
 
 const GithubService = {
 
-    getComits: (owner: string , repoName:string)=>{
-        const url: string = `https://api.github.com/repos/${owner}/${repoName}/commits`;
+    getComits: async (owner: string, repoName: string) => {
+        let result:any = null;
+        try {
+            const url: string = `https://api.github.com/repos/${owner}/${repoName}/commits`;
+            const response = await axios.get(url, {
+                headers: {
+                    Authorization: `token ${GIT_TOKEN}`
+                }
+            });
+            result = response.data;
+
+           
+        } catch (error) {
+           console.log(error);
+        }finally{
+            return result;
+        }
 
 
     },
 }
+
+export default GithubService;
