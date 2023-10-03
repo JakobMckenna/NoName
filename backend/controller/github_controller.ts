fd7d3b99c4d65c85df1cdda6c9e12858cad5f5f3
Tehilah <tkangamba@hotmail.com>
2023-10-03 17:26:06 -0500

    created github controller

---
+++ backend/controller/github_controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import GithubService from '../services/github_service';
+
+const GithubController = {
+    getCommits: async (req: Request, res: Response)=>{
+      const body = req.body;
+      const owner:string = body.owner;
+      const repoName:string = body.name;
+
+      const commits = await GithubService.getComits(owner , repoName);
+
+      if (commits) {
+        // successfully got commits
+        res.status(200).json({ commits: commits });
+    } else {
+        // failed to get commits
+        res.status(400).json({ "message": "failed" })
+    }
+
+    }
+}
