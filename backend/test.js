12778254a221d125968c566639f538abe593575f
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-10-08 14:45:43 -0500

    Added first test

--- backend/test.js
+++ backend/test.js
@@ -1,1 +1,12 @@
 // test suite
+testsPassed = 0;
+console.log("Running tests")
+//test creating a user 
+userData = createUserPassWordData("testUser", "testUser@gmail.com", "testPassword")
+if(userData != null){
+    testsPassed++;
+    console.log("Test passed, user data is: " + userData)
+
+} else {
+    console.log("Failed to create a user")
+}
