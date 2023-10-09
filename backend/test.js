c865cf06b768c28df2be78439d2e9c181c4e34c5
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-10-09 11:53:24 -0500

    Added basic unit test

--- backend/test.js
+++ backend/test.js
@@ -1,14 +1,26 @@
+//import user_model from './backend/user_model';
 // test suite
 // npm install jest -save-dev
-testsPassed = 0;
-console.log("Running tests")
+
+const user_model = require('./user_model')
+
+test('Testing creating user data', () => {
+    expect(createUserPassWordData("testUser", "testUser@gmail.com", "testPassword")).toBe(!null);
+  });
+
+
+
+
+
+
+
 //test creating a user 
 //doesnt work yet, need to access outside files
-userData = createUserPassWordData("testUser", "testUser@gmail.com", "testPassword")
+/*userData = createUserPassWordData("testUser", "testUser@gmail.com", "testPassword")
 if(userData != null){
     testsPassed++;
     console.log("Test passed, user data is: " + userData)
 
 } else {
     console.log("Failed to create a user")
-}
+}/*/
