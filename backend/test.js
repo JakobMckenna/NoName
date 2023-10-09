b2ffe6a7a8e1f1c79666c8310522d77bd1b34d9b
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-10-09 12:14:13 -0500

    Added test instructions

--- backend/test.js
+++ backend/test.js
@@ -8,19 +8,3 @@ test('Testing creating user data', () => {
     expect(createUserPassWordData("testUser", "testUser@gmail.com", "testPassword")).toBe(!null);
   });
 
-
-
-
-
-
-
-//test creating a user 
-//doesnt work yet, need to access outside files
-/*userData = createUserPassWordData("testUser", "testUser@gmail.com", "testPassword")
-if(userData != null){
-    testsPassed++;
-    console.log("Test passed, user data is: " + userData)
-
-} else {
-    console.log("Failed to create a user")
-}/*/
