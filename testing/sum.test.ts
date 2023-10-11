d40d9555f6a9d84435a8a978378e9753d7207491
Tehilah <tehillahk@outlook.com>
2023-10-10 23:33:20 -0500

    added sample test sums example

--- testing/sum.test.ts
+++ testing/sum.test.ts
@@ -1,6 +1,8 @@
+import {describe, expect, test} from '@jest/globals'
+function sum(a:number, b:number) {
+    return a + b;
+  }
 
-describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(3).toBe(3);
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
   });
-});
