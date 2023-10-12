ba63315843dcf8679f0711a4d05dc284a12184b1
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-10-11 23:22:35 -0500

    Added dummy tests

--- testing/user_model.test.ts
+++ testing/user_model.test.ts
@@ -4,3 +4,13 @@ test('user created', async () => {
     const data = await createUserPasswordData("john","john99@email.com","password");
     expect(data?.email).toBe("john99@email.com");
   });
+
+  test('user created', async () => {
+    const data2 = await createUserPasswordData("john","john99@email.com","password");
+    expect(data2).toBe(true);
+  });
+
+  test('user created', async () => {
+    const data3 = await createUserPasswordData("john","john99@email.com","password");
+    expect(data3).toBeTruthy();
+  });
