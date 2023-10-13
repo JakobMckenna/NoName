ce603e0b4398489e1c19d2d8b610eedafba932e3
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-10-13 11:27:26 -0500

    Added tests

--- testing/user_model.test.ts
+++ testing/user_model.test.ts
@@ -1,16 +1,16 @@
 import {createUserPasswordData} from "../backend/data-access/user_model"
 
 test('user created', async () => {
-    const data = await createUserPasswordData("john","john99@email.com","password");
+    const data = await createUserPasswordData("john1","john991@email.com","password1");
     expect(data?.email).toBe("john99@email.com");
   });
 
   test('user created', async () => {
-    const data2 = await createUserPasswordData("john","john99@email.com","password");
+    const data2 = await createUserPasswordData("john2","john992@email.com","password2");
     expect(data2).toBe(true);
   });
 
   test('user created', async () => {
-    const data3 = await createUserPasswordData("john","john99@email.com","password");
-    expect(data3).toBeTruthy();
+    const data3 = await createUserPasswordData("null", "null", "null");
+    expect(data3).toBeNull();
   });
