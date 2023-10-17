a28b0057915bf38f29f48f0bb461d8dc21378c2c
Tehilah <tehillahk@outlook.com>
2023-10-17 14:43:46 -0500

    added notes functionality

--- backend/routes/project_routes.ts
+++ backend/routes/project_routes.ts
@@ -1,29 +1,38 @@
 import express from 'express';
 import ProjectController from '../controller/project_controller';
+import NotesController from '../controller/notes_controller';
 
 const projectRoutes = express.Router();
 
-projectRoutes.get("/",ProjectController.getAllProjects)
+projectRoutes.get("/",ProjectController.getAllProjects);
 
-projectRoutes.get("/:id",ProjectController.getProject)
+projectRoutes.get("/:id",ProjectController.getProject);
 
+projectRoutes.get("/member/:id",ProjectController.getMembers);
 
-projectRoutes.get("/member/:id",ProjectController.getMembers)
+projectRoutes.post("/",ProjectController.createProject);
 
-projectRoutes.post("/",ProjectController.createProject)
+projectRoutes.post("/member",ProjectController.addProjectMember);
 
-projectRoutes.post("/member",ProjectController.addProjectMember)
+projectRoutes.delete("/:id",ProjectController.removeProject);
 
-projectRoutes.delete("/:id",ProjectController.removeProject)
-
-projectRoutes.delete("/member",ProjectController.addProjectMember)
+projectRoutes.delete("/member",ProjectController.addProjectMember);
 
 projectRoutes.post("/repo",ProjectController.addRepo);
 
-projectRoutes.post("/sprint",ProjectController.createSprint)
+projectRoutes.post("/sprint",ProjectController.createSprint);
 
-projectRoutes.delete("/sprint/:id",ProjectController.removeSprint)
+projectRoutes.delete("/sprint/:id",ProjectController.removeSprint);
 
-projectRoutes.get("/sprint/:id",ProjectController.getAllSprints)
+projectRoutes.get("/sprint/:id",ProjectController.getAllSprints);
+
+projectRoutes.get("/notes",NotesController.getAll);
+
+projectRoutes.post("/notes",NotesController.create);
+
+projectRoutes.delete("/notes/:id",NotesController.delete);
+
+projectRoutes.patch("/notes",NotesController.update);
+
 
 export default projectRoutes;
