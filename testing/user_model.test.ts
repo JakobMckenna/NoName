78cf1b35ad8bc989df046281d0b0b98f0d899cae
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-10-16 23:37:40 -0500

    Added unit test

--- testing/user_model.test.ts
+++ testing/user_model.test.ts
@@ -1,16 +1,54 @@
 import {createUserPasswordData} from "../backend/data-access/user_model"
 
 test('user created', async () => {
-    const data = await createUserPasswordData("john1","john991@email.com","password1");
-    expect(data?.email).toBe("john99@email.com");
+    const data3 = await createUserPasswordData("john1","john991@email.com","password1");
+    expect(data3?.email).toBe("john99@email.com");
   });
 
-  test('user created', async () => {
+  /*test('user created', async () => {
     const data2 = await createUserPasswordData("john2","john992@email.com","password2");
     expect(data2).toBe(true);
-  });
+  });*/
 
-  test('user created', async () => {
+  /*test('user created', async () => {
     const data3 = await createUserPasswordData("null", "null", "null");
     expect(data3).toBeNull();
+  });*/
+
+  class FakePrismaClient {
+    async userCreate(data: { name: any; email: any; password: any; }) {
+      // Simulate database logic and return the created user
+      return {
+        id: 1,
+        name: data.name,
+        email: data.email,
+        userPassword: {
+          password: data.password,
+        },
+      };
+    }
+  }
+
+  describe('createUserPasswordData function', () => {
+    it('creates user password data correctly', async () => {
+      // Arrange
+      const fakePrisma = new FakePrismaClient();
+      const userData = {
+        name: 'Test User',
+        email: 'test@example.com',
+        password: 'password',
+      };
+
+      const user = await createUserPasswordData(userData.name, userData.email, userData.password);
+
+    // Assert
+    expect(user).toEqual({
+      id: 1,
+      name: 'Test User',
+      email: 'test@example.com',
+      userPassword: {
+        password: 'password',
+      },
+    });
   });
+});
