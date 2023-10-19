38a2973344e6384a984152f80c20d81d03f2bb92
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-10-19 16:55:02 -0500

    Added unit tests to project_model.test.ts

--- testing/project_model.test.ts
+++ testing/project_model.test.ts
@@ -5,10 +5,18 @@ let userID: number | undefined
 const newUser = createUserPasswordData("ProjectUser", "PU@gmail.com", "Pass")
 //userID = newUser?.id
 
-test('user created', async () => {
+test('project created', async () => {
     let data1 = null
     if(userID !== undefined){
         data1 = await createProject("Project test", 48);
     }
     expect(data1?.name).toBe("Project test");
+  });
+
+  test('project 2 created', async () => {
+    let data2 = null
+    if(userID !== undefined){
+        data2 = await createProject("Project test 2", 48);
+    }
+    expect(data2?.name).toBe("Project test 2");
   });
