da9c79cf493c3c84aebb9b5d6653c60b1ed35c6c
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-10-19 19:38:54 -0500

    Added unit tests

--- testing/project_model.test.ts
+++ testing/project_model.test.ts
@@ -1,10 +1,8 @@
-import {createProject} from "../backend/data-access/project_model"
+import {createProject, getProject} from "../backend/data-access/project_model"
 import {createUserPasswordData, deleteUserByID} from "../backend/data-access/user_model"
 
 let userID: number | undefined
-//const newUser = await createUserPasswordData("ProjectUser", "PU@gmail.com", "Pass");
-//console.log(newUser)
-//userID = newUser.id
+let projID: string | undefined
 
 test('project created', async () => {
   const newUser = await createUserPasswordData("ProjectUser", "PU1@gmail.com", "Pass");
@@ -17,6 +15,7 @@ test('project created', async () => {
     if(userID !== undefined){
         data1 = await createProject("Project test", userID);
     }
+    projID = data1?.id
     expect(data1?.name).toBe("Project test");
   });
 
@@ -26,6 +25,14 @@ test('project created', async () => {
         data2 = await createProject("Project test 2", userID);
     }
     expect(data2?.name).toBe("Project test 2");
+  });
+
+  test('project 2 created', async () => {
+    let data4 = null
+    if(projID !== undefined){
+        data4 = await getProject(projID);
+    }
+    expect(data4?.name).toBe("Project test");
   });
 
   test('delete user by ID', async () => {
