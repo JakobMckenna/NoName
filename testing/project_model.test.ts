24e099f58b2969d679551d081b97a577b3214e8f
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-10-19 19:14:00 -0500

    Added tests for createing a project

--- testing/project_model.test.ts
+++ testing/project_model.test.ts
@@ -1,14 +1,21 @@
 import {createProject} from "../backend/data-access/project_model"
-import {createUserPasswordData} from "../backend/data-access/user_model"
+import {createUserPasswordData, deleteUserByID} from "../backend/data-access/user_model"
 
 let userID: number | undefined
-const newUser = createUserPasswordData("ProjectUser", "PU@gmail.com", "Pass")
-//userID = newUser?.id
+//const newUser = await createUserPasswordData("ProjectUser", "PU@gmail.com", "Pass");
+//console.log(newUser)
+//userID = newUser.id
+
+test('project created', async () => {
+  const newUser = await createUserPasswordData("ProjectUser", "PU1@gmail.com", "Pass");
+  userID = newUser?.id
+  console.log("USERID is " + userID)
+});
 
 test('project created', async () => {
     let data1 = null
     if(userID !== undefined){
-        data1 = await createProject("Project test", 48);
+        data1 = await createProject("Project test", userID);
     }
     expect(data1?.name).toBe("Project test");
   });
@@ -16,7 +23,15 @@ test('project created', async () => {
   test('project 2 created', async () => {
     let data2 = null
     if(userID !== undefined){
-        data2 = await createProject("Project test 2", 48);
+        data2 = await createProject("Project test 2", userID);
     }
     expect(data2?.name).toBe("Project test 2");
+  });
+
+  test('delete user by ID', async () => {
+    let data3 = null
+   if(userID !== undefined){
+     data3 = await deleteUserByID(userID);
+   }
+   expect(data3).toBeNull();
   });
