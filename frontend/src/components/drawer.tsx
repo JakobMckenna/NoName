98ab8dae9b73bf52a7cca0c09829808b00eb54b1
Isabelle Anderson-Gregoire <ianderson.gregoire@gmail.com>
2023-10-20 11:17:54 -0500

    Removed settings link - under construction

--- frontend/src/components/drawer.tsx
+++ frontend/src/components/drawer.tsx
@@ -21,7 +21,7 @@ const Drawer = () => {
                     <li><Link className="text-3xl" href="/">Projects</Link></li>
                     <li><Link className="text-3xl" href="/">Tasks</Link></li>
                     <li><Link className="text-3xl" href="/">Research</Link></li>
-                    <li><Link className="text-3xl" href="/settings">Settings</Link></li>
+                    <li><Link className="text-3xl" href="/">Settings</Link></li> {/* href = "/settings"*/}
                     <li>
                         <a className="text-3xl" onClick={
                             () => {
