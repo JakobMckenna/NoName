4dc9afd0cf1895219b78c74b35d361ec7dc36c3f
lovelyturtles <kihailes@myumanitoba.ca>
2023-11-15 16:41:14 -0600

    Changed to localhost in config file

--- frontend/config.ts
+++ frontend/config.ts
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const backendApiUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:5001';
+// const backendApiUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:5001';
+const backendApiUrl = 'http://localhost:5001';
 
 const config = {
   backendApiUrl
