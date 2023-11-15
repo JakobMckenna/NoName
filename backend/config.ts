2105cea4decb865035ad6ba68c760ac30816db9a
lovelyturtles <kihailes@myumanitoba.ca>
2023-11-14 18:44:37 -0600

    Added config file and local env for client side environment vars

---
+++ backend/config.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const apiUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:5001';
+
+const config = {
+  apiUrl,
+  usersEndpoint: `${apiUrl}/users`,
+};
+
+export default config;
--- backend/index.ts
+++ backend/index.ts
@@ -17,7 +17,7 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
   cors:{
-    origin:"http://localhost:3000"
+    origin:[ "http://frontend:3000", "http://localhost:3000" ]
   }
 });
 
