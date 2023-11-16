aced383e899a4fe5ef3d60ee4cbc298024af565f
lovelyturtles <kihailes@myumanitoba.ca>
2023-11-15 18:09:03 -0600

    Changed backend config file

--- backend/config.ts
+++ backend/config.ts
@@ -2,12 +2,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const apiUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:5001';
-const apiUrl = 'http://localhost:5001';
+// const backendApiUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:5001';
+const backendApiUrl = 'http://localhost:5001';
 
 const config = {
-  apiUrl,
-  usersEndpoint: `${apiUrl}/users`,
+  backendApiUrl
 };
 
 export default config;
