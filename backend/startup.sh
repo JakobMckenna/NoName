8b672a50975ab831bbf5d9bad8afbf1ed5937784
Tehillah <tehillahk@outlook.com>
2023-11-17 02:13:05 -0600

    chnged project member to on client

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 echo "Waiting for MySQL to be ready..."
-# Wait for MySQL to be available
-/usr/local/bin/wait-for-it.sh -t 45 localhost:3306
+
 echo "Waiting for MySQL to be ready..."
 
 # Run Prisma migration
 npx prisma migrate dev --schema prisma/schema.prisma
-npx prisma db push
+
 npm run postinstall
 
 # Build the project
