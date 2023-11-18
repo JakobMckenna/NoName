58c2ff35cc41072dbd2f4eb795160e0758f94e01
Tehilah <tehillahk@outlook.com>
2023-11-18 17:32:02 -0600

    put things back

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,15 +1,19 @@
 /* eslint-disable */
+import { useEffect, useMemo, useState } from 'react';
 import useTheme from "~/hooks/use_theme";
 import Drawer from "./drawer";
 import Link from "next/link";
 
-const setLocal = () => {
-
-}
-
 
 const Navbar = ({ userName }: { userName: string }) => {
   const [theme, setTheme] = useTheme()
+  const [isChecked ,setIsChecked] =  useState<boolean>((theme == "retro"))
+
+  useEffect(
+    ()=>{
+      setIsChecked(theme == "retro")
+    },[isChecked ,theme]
+  )
   return (
     <div className="navbar bg-base-100 mb-5">
       <div className="navbar-start">
@@ -21,25 +25,25 @@ const Navbar = ({ userName }: { userName: string }) => {
       <div className="navbar-end">
         <label className="flex cursor-pointer gap-2">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-
+          
           <input
+            checked={isChecked}
             type="checkbox" value="retro" className="toggle "
             onClick={
               () => {
                 if (setTheme && typeof setTheme !== "string" && (theme == "dark" || theme == null || theme == undefined)) {
-                  //  if (theme=="dark" || theme==null || theme==undefined){
-                  setTheme("retro")
-                  //   }else if(theme=="retro"){
-                  //     setTheme("dark")
-                  //  }
+
+                  setTheme("retro");
+                  setIsChecked(true);
 
                 } else if (setTheme && typeof setTheme !== "string" && theme == "retro") {
-                  setTheme("dark")
+                  setTheme("dark");
+                  setIsChecked(false);
                 }
               }
 
             }
-            checked={theme == null || theme == undefined || theme == "retro"}
+           
 
           />
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
