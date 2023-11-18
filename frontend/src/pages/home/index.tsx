17bdca6eec03b7fba0b6a0927382051565de06dd
Tehilah <tehillahk@outlook.com>
2023-11-17 22:40:55 -0600

    added loading screen

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -15,89 +15,20 @@ import useUser from "~/hooks/use_user";
 
 import config from "config";
 
-function RepoCard({ projects }: any) {
-    //console.log(projects)
+function LoadingTile() {
     return (
-        <div className="card  glass w-80  shadow-xl mr-12 max-h-64 m-4">
-
-            <div className="card-body items-center text-center h-full">
-                <h2 className="card-title">Projects</h2>
-                <ul>
-                    {
-                        projects.map(
-                            (project: any, index: number) => {
-                                return (
-                                    <li key={project.project.id}>
-                                        <Link href={`/project/${project.project.id}`} className="btn   btn-link"> {project.project.name} </Link>
-                                    </li>
-                                )
-                            }
-                        )
-
-                    }
-                </ul>
-                <div className="card-actions justify-start">
-                    <button className="btn btn-secondary  btn-link" onClick={() => {
-                        const modalElement: any = document.getElementById('my_modal_3')
-                        if (modalElement) {
-                            modalElement?.showModal()
-                        }
-
-                    }
-                    }>Add Project</button>
-                </div>
+        <div className="card skeleton w-96 bg-primary glass text-primary-content shadow-xlw-full mb-2 hover:-translate-y-3 hover:-skew-y-3">
+            <div className="card-body">
+                <h2 className="skeleton h-4 w-28"> </h2>
+                <p className="skeleton h-4 w-28"></p>
             </div>
+
         </div>
     )
 }
 
-function TaskCard() {
+function Tile({ id, name, user }: { id: string, name: string, user: string }) {
     return (
-        <div className="card bg-neutral-focus w-80  shadow-xl mr-12 max-h-64 m-4">
-
-            <div className="card-body items-center text-center h-full">
-                <h2 className="card-title">Tasks</h2>
-                <ul>
-
-                </ul>
-                <div className="card-actions justify-start">
-                    <button className="btn btn-secondary  btn-link">Add Task</button>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function StatsCard() {
-    return (
-        <div className="card bg-neutral-focus w-80  shadow-xl mr-12 max-h-64 m-4">
-
-            <div className="card-body items-center text-center h-full">
-                <h2 className="card-title">Statistics</h2>
-                <ul>
-
-                </ul>
-            </div>
-        </div>
-    )
-}
-
-function MenuCard() {
-    return (
-        <div className="card  bg-neutral-focus w-80  shadow-xl mr-12 max-h-56 m-4">
-
-            <div className="card-body items-center text-center h-full">
-                <h2 className="card-title">Research Links</h2>
-                <ul>
-
-                </ul>
-            </div>
-        </div>
-    )
-}
-
-function Tile({id,name,user}:{id:string ,name:string,user:string}){
-    return(
         <Link href={`/project/${id}`} className="card w-96 bg-primary glass text-primary-content shadow-xlw-full mb-2 hover:-translate-y-3 hover:-skew-y-3">
             <div className="card-body">
                 <h2 className="card-title">{name} </h2>
@@ -107,7 +38,7 @@ function Tile({id,name,user}:{id:string ,name:string,user:string}){
         </Link>
     )
 }
-function ProjectHero({projects}:{projects:any}) {
+function ProjectHero({ projects }: { projects: any }) {
     return (
         <div className="hero  bg-base-100">
             <div className="hero-content ">
@@ -115,18 +46,26 @@ function ProjectHero({projects}:{projects:any}) {
                     <h1 className="text-5xl font-bold">Your Projects</h1>
                     <p className="py-6">List of projects</p>
                     {
-                        projects && projects.map(
+                        projects.length > 0 ? projects.map(
                             (project: any, index: number) => {
                                 const id = project.project.id;
-                                const user =project.project.user?.name;
+                                const user = project.project.user?.name;
                                 const projectName = project.project.name;
-                                
+
                                 return (
-                                    
+
                                     <Tile id={id} name={projectName} user={user} />
                                 )
                             }
+                        ) : (
+                            <>
+                                <LoadingTile />
+                                <LoadingTile />
+                                <LoadingTile />
+                            </>
                         )
+
+
 
                     }
                     <button className="btn btn-primary">Get Started</button>
@@ -135,6 +74,7 @@ function ProjectHero({projects}:{projects:any}) {
         </div>
     )
 }
+
 
 export default function LandingPage() {
     //const [loading ,setLoading] = useState(true)
