d4d7c0af24f742ec56994f7df094e9dacb7ca064
Tehilah <tehillahk@outlook.com>
2023-11-18 01:46:08 -0600

    made theme changer

--- frontend/src/hooks/use_theme.tsx
+++ frontend/src/hooks/use_theme.tsx
@@ -1,18 +1,41 @@
 /* eslint-disable */
 import { useEffect, useState } from "react";
 
-const useTheme = () => {
-    const [theme, setTheme] = useState("dark")
+const getItem = () => {
+    let result = "dark"
+    let localTheme :string|null = null;
+    if(typeof window !== 'undefined' && window.localStorage){
+        localTheme = localStorage.getItem("theme");
+    }
+    
+   
+
+    if (localTheme!= null) {
+        result = localTheme;
+    }
+    return result;
+}
+
+
+const useTheme = (defaultTheme:string) => {
+
+    const [theme, setTheme] = useState<string>(defaultTheme)
 
     useEffect(
         () => {
-            localStorage.setItem("theme", theme);
             const localTheme = localStorage.getItem("theme");
+            localStorage.setItem("theme", theme);
+
             // add custom data-theme attribute to html tag required to update theme using DaisyUI
             const element = document.querySelector("html")
             if (localTheme) {
-                element?.setAttribute("data-theme", localTheme)
+                element?.setAttribute("data-theme", theme)
+               // setTheme(localTheme)
+            } else {
+                element?.setAttribute("data-theme", "dark")
+               // setTheme("dark")
             }
+            
 
 
         }
