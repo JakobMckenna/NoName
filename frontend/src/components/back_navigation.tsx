383121a43ccede75017eec5fbbd68ca23d993e97
Tehilah <tehillahk@outlook.com>
2023-11-18 23:50:22 -0600

    added back to home page button

---
+++ frontend/src/components/back_navigation.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const BackPage = ({ link, name }: { link: string, name: string }) => {
+    return (
+        <Link href={link} className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mb-5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M9.53 2.47a.75.75 0 010 1.06L4.81 8.25H15a6.75 6.75 0 010 13.5h-3a.75.75 0 010-1.5h3a5.25 5.25 0 100-10.5H4.81l4.72 4.72a.75.75 0 11-1.06 1.06l-6-6a.75.75 0 010-1.06l6-6a.75.75 0 011.06 0z" clipRule="evenodd" />
+            </svg>
+            {name}
+        </Link>
+    );
+}
+
+export default BackPage;
--- frontend/src/pages/project/[slug].tsx
+++ frontend/src/pages/project/[slug].tsx
@@ -12,6 +12,7 @@ import Head from "next/head";
 
 import config from "config";
 import DeleteModal from "~/components/delete_project_modal";
+import BackPage from "~/components/back_navigation";
 
 function MenuCard({ github, projectID }: { github: any, projectID: string }) {
     if (github === null) {
@@ -110,6 +111,8 @@ function ChatCard({ projectID }: { projectID: string }) {
 }
 
 
+
+
 export default function Project() {
     const router = useRouter();
     const [user, loading] = useUser();
@@ -133,8 +136,8 @@ export default function Project() {
             return results.data.projects
 
         } catch (error) {
-           // router.push("/home")
-           //
+            // router.push("/home")
+            //
         }
 
     }
@@ -178,27 +181,31 @@ export default function Project() {
                 <main className="container mb-10  ">
                     <div className="flex flex-col justify-center items-center">
                         <div>
-                            <div className="flex flex-row items-center " >
-                                <h1 className="text-4xl uppercase mb-2">{projectData?.name} PROJECT</h1>
-                                <div className="tooltip tooltip-right" data-tip="Delete Project">
-                                    <button
-                                        className=" btn btn-primary   
+                            <div className=" " >
+                                <BackPage link="/home" name="Back To Home Page" />
+                                <div className="flex flex-row items-center ">
+
+                                    <h1 className="text-4xl uppercase mb-2">{projectData?.name} PROJECT</h1>
+                                    <div className="tooltip tooltip-right" data-tip="Delete Project">
+                                        <button
+                                            className=" btn btn-primary   
                                     btn-square btn-outline btn-sm ml-5"
-                                        onClick={
-                                            () => {
-                                                const modal: any = document.getElementById('del_proj');
-                                                if (modal) {
-                                                    modal?.showModal();
+                                            onClick={
+                                                () => {
+                                                    const modal: any = document.getElementById('del_proj');
+                                                    if (modal) {
+                                                        modal?.showModal();
+                                                    }
                                                 }
                                             }
-                                        }
-                                    >
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                                            <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clipRule="evenodd" />
-                                        </svg>
+                                        >
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                                <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clipRule="evenodd" />
+                                            </svg>
 
 
-                                    </button>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                             <div>
