cf57b0c127409ebfef1452feb4e37b4e12401542
Tehilah <tehillahk@outlook.com>
2023-11-19 09:57:36 -0600

    made css less verbose on project page

--- frontend/src/pages/project/[slug].tsx
+++ frontend/src/pages/project/[slug].tsx
@@ -111,41 +111,42 @@ function ChatCard({ projectID }: { projectID: string }) {
 
 function Header({ projectData, userID, owner }: { projectData: any, userID: number | null, owner: number | null }) {
     return (
-        <div className=" flex  justify-start    prose  ">
-            <div className="flex flex-col ">
-                <div className="flex flex-row items-baseline ">
-                    <div className="flex flex-col md:flex-row items-baseline">
-                       
-                        <h1 className="uppercase mb-2">{projectData ? `${projectData.name} PROJECT` : (<div className="skeleton h-9 w-72"></div>)} </h1>
-                        {
-                            userID && owner && (<div className="tooltip tooltip-right" data-tip={userID === owner ? "Delete Project" : "Only Owner Can Delete This Project"}>
-                                <button
-                                    className=" btn btn-primary   
+        <div className="ml-12 mt-3 md:ml-0 md:mt-0  prose  ">
+             <BackPage link="/home" name="Back To Home Page" />
+            <div className="flex flex-col justify-start ">
+               
+                <div className="flex flex-col md:flex-row items-baseline">
+
+                    <h1 className="uppercase mb-2">{projectData ? `${projectData.name} PROJECT` : (<div className="skeleton h-9 w-72"></div>)} </h1>
+                    {
+                        userID && owner && (<div className="tooltip tooltip-right" data-tip={userID === owner ? "Delete Project" : "Only Owner Can Delete This Project"}>
+                            <button
+                                className=" btn btn-primary   
                                     btn-square btn-outline btn-sm ml-5"
-                                    onClick={
-                                        () => {
-                                            const modal: any = document.getElementById('del_proj');
-                                            if (modal) {
-                                                modal?.showModal();
-                                            }
+                                onClick={
+                                    () => {
+                                        const modal: any = document.getElementById('del_proj');
+                                        if (modal) {
+                                            modal?.showModal();
                                         }
-
                                     }
-                                    disabled={userID !== owner}
-                                >
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                                        <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clipRule="evenodd" />
-                                    </svg>
+
+                                }
+                                disabled={userID !== owner}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clipRule="evenodd" />
+                                </svg>
 
 
-                                </button>
+                            </button>
 
-                            </div>)
-                        }
-                    </div>
-
-
+                        </div>)
+                    }
                 </div>
+
+
+
                 <p className="text-xl mb-5">Add Sprint to access research notes and setup github to see commits</p>
             </div>
         </div>
@@ -228,7 +229,7 @@ export default function Project() {
             <Navbar userName={`${user?.name}#${user?.id}`} />
 
             <main className="flex flex-col items-center justify-center  mb-10    ">
-                <BackPage link="/home" name="Back To Home Page" />
+
                 <Header projectData={projectData} userID={userID} owner={owner} />
                 <div className=" grid grid-cols-1 md:grid-cols-2 gap-y-8 gap-x-5 pl-5 md:pl-24  ">
 
