1a6d05aed12c5ce2025547006c425ab91cd11e2e
Tehilah <tehillahk@outlook.com>
2023-11-21 23:16:35 -0600

    make search

--- frontend/src/pages/project/research/[slug].tsx
+++ frontend/src/pages/project/research/[slug].tsx
@@ -96,30 +96,72 @@ function NoteList({ list, remove, refresh }: { list: any, remove: any, refresh: 
     )
 }
 
-
+function SearchBar({ search, filter, milestone, topic, sprints, changeMilestone, reset }: { search: any, filter: any, milestone: string, topic: string, sprints: any, changeMilestone: any, reset: any }) {
+    return (
+        <div className="flex flex-col md:flex-row justify-between w-full md:w-3/4">
+            <input
+                type="text"
+                placeholder="search for bookmark"
+                className="input input-bordered w-full max-w-xs mr-5"
+                onChange={
+                    (event: React.ChangeEvent<HTMLInputElement>) => {
+                        const topicTxt = event.target.value;
+                        filter(topicTxt, milestone);
+                    }
+                }
+            />
+            <select
+                className="select select-bordered w-full max-w-xs"
+                onChange={
+                    (event: React.ChangeEvent<HTMLSelectElement>) => {
+                        const milestoneTxt = event.target.value;
+                        changeMilestone(milestoneTxt)
+                    }
+                }
+            >
+                <option disabled selected>Filter by Sprint</option>
+                {
+                    sprints?.map(
+                        (sprint: any) => (
+                            <option key={sprint.id} value={sprint.id}>{sprint.name}</option>
+                        )
+                    )
+                }
+            </select>
+            <div className="flex flex-row w-1/5 justify-between ">
+                <button className="btn  btn-accent  " onClick={() => reset()}>Search</button>
+                <button className="btn" onClick={() => reset()}>reset filter</button>
+            </div>
+        </div>
+    )
+}
 
 export default function Research() {
     const router = useRouter()
     const projectID: string = String(router.query.slug);
-    const [user, loading] = useUser()
-    const [notes, setNotes] = useState<any[] | null>(null)
+    const [user, loading] = useUser();
+    const [notes, setNotes] = useState<any[] | null>(null);
+    const [filteredNotes, setFilteredNotes] = useState<any[]>([])
     // const [notes, changeID] = useNotes()
-    const [sprints, setID] = useSprint()
-    const [refresh, setRefresh] = useState(true)
+    const [sprints, setID] = useSprint();
+    const [refresh, setRefresh] = useState(true);
+    const [searchTopic, setSearchTopic] = useState<string>("")
+    const [searchMilestone, setSearchMilestone] = useState<string>("")
 
     const getResponse = async () => {
         try {
-            const reqUrl = `${config.backendApiUrl}/projects/notes/${projectID}`
-            const results = await axios.get(reqUrl)
+            const reqUrl = `${config.backendApiUrl}/projects/notes/${projectID}`;
+            const results = await axios.get(reqUrl);
 
-            console.log(results.data)
-            setNotes(results.data.notes)
-            setRefresh(false)
+            console.log(results.data);
+            setNotes(results.data.notes);
+            setFilteredNotes(results.data.notes);
+            setRefresh(false);
 
         } catch (error) {
             //we failed to get notes for some reason
             //setNotes(null);
-            setRefresh(true)
+            setRefresh(true);
 
         }
     }
@@ -134,6 +176,51 @@ export default function Research() {
             setNotes(noteList);
     }
 
+
+    const search = () => {
+
+        let results = notes?.filter((note) => {
+            return note.sprintID == searchMilestone
+        })
+        if (results) {
+            setFilteredNotes(results);
+        }
+
+    }
+
+    const filter = (topic: string, milestone: string) => {
+
+
+        const results = filteredNotes.filter((note) => {
+            return note.title.toLowerCase().includes(topic.toLowerCase()) || note.details.toLowerCase().includes(topic.toLowerCase());
+        });
+        if (results) {
+            setFilteredNotes(results);
+        }
+    }
+
+    const changeMilestone = (milestone: string) => {
+        // setSearchMilestone(milestone);
+        if (filteredNotes.length > 0) {
+            const results = filteredNotes.filter((note) => {
+                return note.sprintID == milestone;
+            })
+
+            setFilteredNotes(results);
+        } else if (filteredNotes.length == 0 && notes) {
+            const results = notes.filter((note) => {
+                return note.sprintID == milestone;
+            })
+
+            setFilteredNotes(results);
+        }
+
+    }
+
+    const reset = () => {
+        if (notes)
+            setFilteredNotes(notes)
+    }
     useEffect(
         () => {
             if (user != null && projectID != null && setID != null) {
@@ -154,19 +241,27 @@ export default function Research() {
             <Navbar userName={`${user?.name}#${user?.id}`} />
             <main className="container mx-auto px-7 ">
                 {!refresh ? (<BackPage link={`/project/${projectID}`} name={`Back to Project page`} />) : (<div className="skeleton h-9 w-96 mb-5"></div>)}
-                <div className="flex flex-row mx-auto row-gap-2 mb-10">
-                    <button onClick={
-                        () => {
-                            const modalElement: any = document.getElementById('my_modal_2')
-                            modalElement.showModal()
-                            setRefresh(true)
-                        }
-                    } className="btn btn-primary">Add Note</button>
+                <div className="flex flex-col mx-auto row-gap-2 mb-10">
+                    <div className="flex flex-row justify-between mb-5">
+                        <button
+                            onClick={
+                                () => {
+                                    const modalElement: any = document.getElementById('my_modal_2')
+                                    modalElement.showModal()
+                                    setRefresh(true)
+                                }
+                            }
+                            className="btn btn-primary"
+                        >
+                            Add Note
+                        </button>
+                    </div>
+                    <SearchBar sprints={sprints} filter={filter} search={search} changeMilestone={changeMilestone} milestone={searchMilestone} topic={searchTopic} reset={reset} />
 
                 </div>
 
                 <div className="container  mx-auto mb-24">
-                    {notes != null ? (<NoteList list={notes} remove={removeNotes} refresh={(val: boolean) => setRefresh(val)} />) : (<Spinner />)}
+                    {notes != null ? (<NoteList list={filteredNotes} remove={removeNotes} refresh={(val: boolean) => setRefresh(val)} />) : (<Spinner />)}
                 </div>
             </main>
             <NotesModal projectID={projectID} addNotes={addNotes} userID={user?.id} sprints={sprints} refresh={(val: boolean) => setRefresh(val)} />
