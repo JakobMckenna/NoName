7ec6facfb093671343506e30fdb970e7592d0e86
Tehilah <tehillahk@outlook.com>
2023-11-24 16:59:50 -0600

    added animation

--- frontend/src/pages/project/sprint/[slug].tsx
+++ frontend/src/pages/project/sprint/[slug].tsx
@@ -11,8 +11,9 @@ import useUser from "~/hooks/use_user";
 import config from "config";
 import BackPage from "~/components/back_navigation";
 import SprintDelete from "~/components/delete_sprint_modal";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
-function Sprints({ sprints, changeSelected }: any) {
+function Sprints({ sprints, changeSelected, animate }: any) {
     // console.log(sprints)
     const convDate = (date: string) => {
         const result = new Date(date)
@@ -22,47 +23,50 @@ function Sprints({ sprints, changeSelected }: any) {
         <div className="flex flex-col">
 
             <h1 className="text-2xl text-center uppercase mb-2 ">Sprints</h1>
-            {
-                sprints && sprints.map(
-
-                    (sprint: any) => {
-                        const start = convDate(sprint.start)
-                        const end = convDate(sprint.deadline)
-                        return (<div key={sprint.id} className="collapse collapse-arrow w-80 bg-base-200">
-                            <input type="radio" name="my-accordion-2" />
-                            <div className="collapse-title capitalize text-xl font-medium">
-                                {sprint.name}
-                            </div>
-                            <div className="collapse-content">
-                                <button
-                                    className="btn  btn-primary"
-                                    onClick={
-                                        () => {
-                                            changeSelected(sprint.id)
-                                            const modal: any = document.getElementById('del_sprint')
-                                            if (modal) {
-                                                modal?.showModal()
+            <ul ref={animate}>
+                {
+                    sprints && sprints.map(
+                        (sprint: any) => {
+                            const start = convDate(sprint.start)
+                            const end = convDate(sprint.deadline)
+                            return (
+                                <li key={sprint.id} className="collapse collapse-arrow w-80 bg-base-200">
+                                    <input type="radio" name="my-accordion-2" />
+                                    <div className="collapse-title capitalize text-xl font-medium">
+                                        {sprint.name}
+                                    </div>
+                                    <div className="collapse-content">
+                                        <button
+                                            className="btn  btn-primary"
+                                            onClick={
+                                                () => {
+                                                    changeSelected(sprint.id)
+                                                    const modal: any = document.getElementById('del_sprint')
+                                                    if (modal) {
+                                                        modal?.showModal()
+                                                    }
+                                                }
                                             }
-                                        }
-                                    }
 
 
-                                >
-                                    Delete Sprint
-                                </button>
-                                <div className="flex flex-row justify-between">
-                                    <p>start: {start}</p>
-                                    <p>end: {end}</p>
-                                </div>
-                            </div>
-                        </div>)
-                    })
-            }
+                                        >
+                                            Delete Sprint
+                                        </button>
+                                        <div className="flex flex-row justify-between">
+                                            <p>start: {start}</p>
+                                            <p>end: {end}</p>
+                                        </div>
+                                    </div>
+                                </li>
+                            )
+                        })
+                }
+            </ul>
         </div>
     )
 }
 
-function MilestoneHero({ sprints, changeSelected }: any) {
+function MilestoneHero({ sprints, changeSelected, animate }: any) {
     return (
         <div className="flex  flex-col justify-evenly md:flex-row ">
             <div className="card  bg-neutral-focus w-80  shadow-xl h-56 ">
@@ -84,7 +88,7 @@ function MilestoneHero({ sprints, changeSelected }: any) {
                 </div>
 
             </div>
-            <Sprints sprints={sprints} changeSelected={changeSelected} />
+            <Sprints sprints={sprints} changeSelected={changeSelected} animate={animate} />
         </div>
     )
 }
@@ -96,7 +100,8 @@ export default function SprintPage() {
     const [sprints, setSprints] = useState<any[]>([]);
     const projectID = router.query.slug;
     const [refresh, setRefesh] = useState(true);
-    const [selected, setSelected] = useState<string>("")
+    const [selected, setSelected] = useState<string>("");
+    const [parent, enableAnimations] = useAutoAnimate({duration:300});
     const isRefresh = () => {
         return refresh === true;
     }
@@ -129,20 +134,19 @@ export default function SprintPage() {
         setSelected(sprintID);
     }
 
-    const removeSprint =(sprintID:string)=>{
-        const sprintList = sprints.filter((sprint) =>sprint.id !== sprintID);
-        if (sprintList )
-        {
-            setSprints(sprintList ); 
+    const removeSprint = (sprintID: string) => {
+        const sprintList = sprints.filter((sprint) => sprint.id !== sprintID);
+        if (sprintList) {
+            setSprints(sprintList);
         }
-        
+
     }
 
     const deleteSprint = async () => {
         try {
 
             const responseSprint = await axios.delete(`${config.backendApiUrl}/projects/sprint/${selected}`);
-            const sprint =responseSprint.data.sprint;
+            const sprint = responseSprint.data.sprint;
             // add(responseSprint.data.sprint)
             removeSprint(selected)
             return responseSprint.data.sprint;
@@ -150,39 +154,39 @@ export default function SprintPage() {
         } catch (error) {
             console.log(error)
         }
-    
-
-}
-
-useEffect(
-    () => {
-        if (projectID != null && refresh) {
-
-            getSprints();
-
-        }
 
 
-    }, [sprints, projectID]
+    }
 
-)
-return (
-    <div>
-        <Head>
-            <title>Milestones | Dev Diaries</title>
-            <meta name="description" content="Generated by create-t3-app" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Navbar userName={`${user?.name}#${user?.id}`} />
-        <main className="container mx-auto">
-            {projectID != null ? (<BackPage link={`/project/${projectID as string}`} name={`Back to  Project page`} />) : (<div className="skeleton h-9 w-96 mb-5"></div>)}
+    useEffect(
+        () => {
+            if (projectID != null && refresh) {
 
-            <div >
-                <MilestoneHero sprints={sprints} changeSelected={changeSelected} />
-            </div>
-        </main>
-        <SprintModal projectID={projectID as string} add={addSprint} />
-        <SprintDelete deleteSprint={deleteSprint} />
-    </div>
-)
+                getSprints();
+
+            }
+
+
+        }, [sprints, projectID]
+
+    )
+    return (
+        <div>
+            <Head>
+                <title>Milestones | Dev Diaries</title>
+                <meta name="description" content="Generated by create-t3-app" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Navbar userName={`${user?.name}#${user?.id}`} />
+            <main className="container mx-auto">
+                {projectID != null ? (<BackPage link={`/project/${projectID as string}`} name={`Back to  Project page`} />) : (<div className="skeleton h-9 w-96 mb-5"></div>)}
+
+                <div ref={parent}>
+                    <MilestoneHero sprints={sprints} changeSelected={changeSelected} animate={parent} />
+                </div>
+            </main>
+            <SprintModal projectID={projectID as string} add={addSprint} />
+            <SprintDelete deleteSprint={deleteSprint} />
+        </div>
+    )
 }
