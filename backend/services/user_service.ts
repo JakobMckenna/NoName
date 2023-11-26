c9f9b06ed44019dbf66d4288dd640fe33602ea29
Tehilah <tehillahk@outlook.com>
2023-11-26 04:04:34 -0600

    comments on users and notes

--- backend/services/user_service.ts
+++ backend/services/user_service.ts
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview user service this handle the business logic of users
+ */
+
 
 import { createUserPasswordData, deleteUserByID, getAllUsers, getUserPassword, getUserProjects } from '../data-access/user_model';
 import jwt from "jsonwebtoken";
@@ -14,13 +18,20 @@ const createUserToken = (email: string) => {
 
 const UserService = {
 
+  /**
+   * sign in
+   * handles user authentication by confirming user password
+   * @param email 
+   * @param password 
+   * @returns  user object
+   */
   signIn: async (email: string, password: string) => {
     let result: any = null;
     try {
       const user = await getUserPassword(email);
       //if reul
       if (password == user?.userPassword?.password) {
-        console.log("password correct");
+        //console.log("password correct");
         result = {
           id: user.id,
           email: user.email,
@@ -38,6 +49,15 @@ const UserService = {
       return result;
     }
   },
+
+  /**
+   * createUser
+   * creates User and their related data
+   * @param email 
+   * @param name 
+   * @param password 
+   * @returns  user object
+   */
   createUser: async (email: string, name: string, password: string) => {
     let result: any = null;
     try {
@@ -46,7 +66,7 @@ const UserService = {
         result = {
           id: user?.id,
           email: user?.email,
-          name:user?.name,
+          name: user?.name,
           token: createUserToken(user?.email)
         }
       }
@@ -56,6 +76,13 @@ const UserService = {
       return result;
     }
   },
+
+  /**
+   * deleteUser
+   * deletes user by ID
+   * @param userID 
+   * @returns  user object
+   */
   deleteUser: async (userID: number) => {
     let result: any = null;
     try {
@@ -71,6 +98,13 @@ const UserService = {
       return result;
     }
   },
+
+  /**
+   * getProjects
+   * gets user project ID
+   * @param userID 
+   * @returns  a list of projects
+   */
   getProjects: async (userID: number) => {
     let result: any = null;
     try {
@@ -84,7 +118,13 @@ const UserService = {
       return result;
     }
   },
-  getAll: async()=>{
+
+  /**
+   * getAll
+   * gets all users
+   * @returns  a list of user objects
+   */
+  getAll: async () => {
     let result: any = null;
     try {
       const users = await getAllUsers();
