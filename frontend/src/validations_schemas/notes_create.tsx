6ea92efded70f4295b097057d394edae080c0939
Isabelle Anderson-Gregoire <ianderson.gregoire@gmail.com>
2023-12-01 10:25:35 -0600

    Updated research URL validation to check for all invalid urls

--- frontend/src/validations_schemas/notes_create.tsx
+++ frontend/src/validations_schemas/notes_create.tsx
@@ -4,6 +4,7 @@ export const notesValidation = object({
     sprint:string().required("sprint is required"),
     title: string().min(4, "Title name must atleast be 4 characters").required("name is required"),
     details:string().max(80,"use no more than 80 characters in details").required("details is required"),
-    url:string().matches(/^(http|https):\/\//, 'URL must begin with \'http://\' or \'https://\'').required("url is required")
+    url:string().transform((value) => (value ? value.trim() : value)).matches(/^(https?:\/\/[\w\.-]+\.\w{2,})(\/\S*)?$/, "Invalid URL")
+.required("url is required"),
 
 }).required()
