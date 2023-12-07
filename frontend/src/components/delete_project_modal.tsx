1ca0e02d023961717224be102ece8247f5f6bc68
Tehillah Kangamba <56933368+TehillahK@users.noreply.github.com>
2023-12-07 11:05:17 -0600

    Update delete_project_modal.tsx typo

--- frontend/src/components/delete_project_modal.tsx
+++ frontend/src/components/delete_project_modal.tsx
@@ -57,7 +57,7 @@ const DeleteModal = ({ projectID, home }: { projectID: string, home: any }) => {
                         {deleting&&(
                             <Spinner />
                         )}
-                        {deleting?"delting":"yes"}
+                        {deleting?"deleting":"yes"}
                     </button>
                     <button
                         className="btn btn-neutral btn-lg"
