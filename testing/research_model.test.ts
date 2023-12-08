7252d9631d12e9f98747295dd9e1fbf8a28152fd
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-12-08 10:57:08 -0600

    Added tearDown tests and fixed an issue with project name

--- testing/research_model.test.ts
+++ testing/research_model.test.ts
@@ -1,7 +1,7 @@
 import {createUserPasswordData, deleteUserByID} from "../backend/data-access/user_model"
 import {createProject, removeProject, getProject} from "../backend/data-access/project_model"
 import {createRepo} from "../backend/data-access/repo_model"
-import {createSprint} from "../backend/data-access/sprint_model"
+import {createSprint, removeSprint} from "../backend/data-access/sprint_model"
 import {getResearchNotes, createResearchNote, updateResearchNote, deleteResearchNote} from "../backend/data-access/research_model"
 
 let userID: number | undefined
@@ -14,7 +14,7 @@ let sprints = null
 //setup a sample user, project, repo, and set up a sprint
 //this is needed so that we can make research notes
 test('Creating a dummy user to create a project', async () => {
-    const newUser = await createUserPasswordData("ProjectUser3", "PU13@gmail.com", "Pass3");
+    const newUser = await createUserPasswordData("ProjectUser31", "PU131@gmail.com", "Pass31");
     userID = newUser?.id
   });
   
@@ -22,16 +22,16 @@ test('Creating a dummy user to create a project', async () => {
   test('New project created', async () => {
       let data1 = null
       if(userID !== undefined){
-          data1 = await createProject("Project test 23", userID);
+          data1 = await createProject("Project test 231", userID);
       }
       projID = data1?.id
-      expect(data1?.name).toBe("Project test 23");
+      expect(data1?.name).toBe("Project test 231");
     });
 
     test('Create a new repo', async () => {
         let data5 = null
         if(projID !== undefined){
-            data5 = await createRepo(projID, "JakobMcKenna","3040-A3");
+            data5 = await createRepo(projID, "JakobMcKenna","COMP3020-milestone-3-main");
         }
         expect(data5).toBeTruthy();
       });
@@ -44,4 +44,34 @@ test('Creating a dummy user to create a project', async () => {
         sprints = data6
         sprintID = data6?.id
         expect(data6).toBeTruthy();
+      });
+
+
+
+      //clean up the DB
+      test('Remove a sprint', async () => {
+        let data10 = null
+       if(sprintID !== undefined){
+         data10 = await removeSprint(sprintID);
+       }
+       expect(data10).toBeTruthy();
+      });
+
+    test('Remove a project', async () => {
+        let data7 = null
+        if(projID !== undefined){
+            data7 = await removeProject(projID);
+        }
+        if(projID !== undefined){
+          data7 = await getProject(projID);
+      }
+        expect(data7?.name).toBe(undefined);
+      });
+    
+      test('delete user by ID', async () => {
+        let data3 = null
+       if(userID !== undefined){
+         data3 = await deleteUserByID(userID);
+       }
+       expect(data3).toBeNull();
       });
