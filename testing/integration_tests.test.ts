dc66ea74491d4ca29458c78f764fce58d3eed8d7
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-12-08 12:28:11 -0600

    Fixed test timeout errors and added new integration test

--- testing/integration_tests.test.ts
+++ testing/integration_tests.test.ts
@@ -1,4 +1,5 @@
-import {createUserPasswordData, deleteUserByID, getAllUsers, getUserPassword} from "../backend/data-access/user_model"
+import {createUserPasswordData, deleteUserByID, getUserProjects, getUserPassword} from "../backend/data-access/user_model"
+import {createProject, getProject, updateProject, removeProject, getAllProjects, removeProjectMember, addProjectMember, getProjectMembers} from "../backend/data-access/project_model"
 
 test('Create and delete user', async () => {
     let userID: number | undefined
@@ -21,6 +22,40 @@ test('Create and delete user', async () => {
 
     const data5 = await getUserPassword("test124@email.com");
     expect(data5?.userPassword?.password).toBe("testpass24");
+
+    let data2 = null
+    if(userID !== undefined){
+        data2 = await deleteUserByID(userID);
+      }
+      expect(data2).toBeNull();
+  });
+
+  test('Make project and get users project', async () => {
+    let userID: number | undefined
+    let projID: string | undefined
+
+    const data1 = await createUserPasswordData("test user 1241","test1241@email.com","testpass241");
+    userID = data1?.id
+    expect(data1?.email).toBe("test1241@email.com");
+
+    const data5 = await getUserPassword("test1241@email.com");
+    expect(data5?.userPassword?.password).toBe("testpass241");
+
+    let data6 = null
+        if(userID !== undefined){
+            data6 = await createProject("Project tester", userID);
+        }
+        projID = data6?.id
+        expect(data6?.name).toBe("Project tester");
+
+    let data7 = null
+    if(projID !== undefined){
+        data7 = await removeProject(projID);
+    }
+    if(projID !== undefined){
+      data7 = await getProject(projID);
+  }
+    expect(data7?.name).toBe(undefined);
 
     let data2 = null
     if(userID !== undefined){
