b9c5dcb5a8ca17788bf00ca4457936da7d62b5b9
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-12-08 12:11:20 -0600

    Add user info integration test

--- testing/integration_tests.test.ts
+++ testing/integration_tests.test.ts
@@ -1,10 +1,26 @@
-import {createUserPasswordData, deleteUserByID} from "../backend/data-access/user_model"
+import {createUserPasswordData, deleteUserByID, getAllUsers, getUserPassword} from "../backend/data-access/user_model"
 
 test('Create and delete user', async () => {
     let userID: number | undefined
     const data1 = await createUserPasswordData("test user 1","test1@email.com","testpass");
     userID = data1?.id
     expect(data1?.email).toBe("test1@email.com");
+
+    let data2 = null
+    if(userID !== undefined){
+        data2 = await deleteUserByID(userID);
+      }
+      expect(data2).toBeNull();
+  });
+
+  test('Create and delete user, and get user information', async () => {
+    let userID: number | undefined
+    const data1 = await createUserPasswordData("test user 124","test124@email.com","testpass24");
+    userID = data1?.id
+    expect(data1?.email).toBe("test124@email.com");
+
+    const data5 = await getUserPassword("test124@email.com");
+    expect(data5?.userPassword?.password).toBe("testpass24");
 
     let data2 = null
     if(userID !== undefined){
