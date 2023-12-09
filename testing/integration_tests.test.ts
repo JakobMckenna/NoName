981ffa1675685aaac12fe6d43f78a1e88d6eef43
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-12-09 16:47:37 -0600

    Added repo model test

--- testing/integration_tests.test.ts
+++ testing/integration_tests.test.ts
@@ -1,5 +1,6 @@
 import {createUserPasswordData, deleteUserByID, getUserProjects, getUserPassword} from "../backend/data-access/user_model"
 import {createProject, getProject, updateProject, removeProject, getAllProjects, removeProjectMember, addProjectMember, getProjectMembers} from "../backend/data-access/project_model"
+import {getRepo, createRepo, updateRepo} from "../backend/data-access/repo_model"
 
 test('Create and delete user', async () => {
     let userID: number | undefined
@@ -63,3 +64,64 @@ test('Create and delete user', async () => {
       }
       expect(data2).toBeNull();
   });
+
+  test('Make project and add a repo', async () => {
+    let userID: number | undefined
+    let projID: string | undefined
+    let repoID: string | undefined
+
+    const data1 = await createUserPasswordData("test user 1241","test1241@email.com","testpass241");
+    userID = data1?.id
+    expect(data1?.email).toBe("test1241@email.com");
+
+        let data6 = null
+        if(userID !== undefined){
+            data6 = await createProject("Project tester", userID);
+        }
+        projID = data6?.id
+        expect(data6?.name).toBe("Project tester");
+
+        test('Create a new repo', async () => {
+          let data5 = null
+          if(projID !== undefined){
+              data5 = await createRepo(projID, "JakobMcKenna","NoName");
+          }
+          expect(data5).toBeTruthy();
+        });
+  
+        test('Get the new repo', async () => {
+          let data6 = null
+          if(projID !== undefined){
+              data6 = await getRepo(projID);
+          }
+          repoID = data6?.id
+          expect(data6).toBeTruthy();
+        });
+  
+        test('Update a non-existant repo', async () => {
+          let data7 = null
+          if(projID !== undefined && repoID !== undefined){
+              data7 = await updateRepo(repoID, projID, "JakobMcKenna", "NoNameExists");
+          }
+          expect(data7).toBeNull();
+        });
+
+
+    let data7 = null
+    if(projID !== undefined){
+        data7 = await removeProject(projID);
+    }
+    if(projID !== undefined){
+      data7 = await getProject(projID);
+  }
+    expect(data7?.name).toBe(undefined);
+
+    let data2 = null
+    if(userID !== undefined){
+        data2 = await deleteUserByID(userID);
+      }
+      expect(data2).toBeNull();
+  });
+
+
+ 
