0c219130718f932961e4cb6685f4c29a92cde543
JakobMckenna <90879131+JakobMckenna@users.noreply.github.com>
2023-12-09 17:00:45 -0600

    Added/fixed tests

--- testing/integration_tests.test.ts
+++ testing/integration_tests.test.ts
@@ -65,60 +65,64 @@ test('Create and delete user', async () => {
       expect(data2).toBeNull();
   });
 
-  test('Make project and add a repo', async () => {
-    let userID: number | undefined
-    let projID: string | undefined
-    let repoID: string | undefined
 
-    const data1 = await createUserPasswordData("test user 1241","test1241@email.com","testpass241");
-    userID = data1?.id
-    expect(data1?.email).toBe("test1241@email.com");
+  let userID1: number | undefined
+  let projID1: string | undefined
+  let repoID1: string | undefined
+
+  test('Make project and add a repo', async () => {
+    
+
+    const data1 = await createUserPasswordData("test user 11241","1test1241@email.com","1testpass241");
+    userID1 = data1?.id
+    expect(data1?.email).toBe("1test1241@email.com");
 
         let data6 = null
-        if(userID !== undefined){
-            data6 = await createProject("Project tester", userID);
+        if(userID1 !== undefined){
+            data6 = await createProject("Project tester 2", userID1);
         }
-        projID = data6?.id
-        expect(data6?.name).toBe("Project tester");
+        projID1 = data6?.id
+        expect(data6?.name).toBe("Project tester 2");
 
-        test('Create a new repo', async () => {
-          let data5 = null
-          if(projID !== undefined){
-              data5 = await createRepo(projID, "JakobMcKenna","NoName");
+          let data51 = null
+          if(projID1 !== undefined){
+              data51 = await createRepo(projID1, "JakobMcKenna","webScapingTutorial");
           }
-          expect(data5).toBeTruthy();
-        });
+          expect(data51).toBeTruthy();
+      
+
+          let data61 = null
+          if(projID1 !== undefined){
+              data61 = await getRepo(projID1);
+          }
+          repoID1 = data61?.id
+          expect(data61).toBeTruthy();
+   
   
-        test('Get the new repo', async () => {
-          let data6 = null
-          if(projID !== undefined){
-              data6 = await getRepo(projID);
+
+          let data71 = null
+          if(projID1 !== undefined && repoID1 !== undefined){
+              data71 = await updateRepo(repoID1, projID1, "JakobMcKenna", "NoNameExists");
           }
-          repoID = data6?.id
-          expect(data6).toBeTruthy();
-        });
+          expect(data71).toBeNull();
+  });
+
+
   
-        test('Update a non-existant repo', async () => {
-          let data7 = null
-          if(projID !== undefined && repoID !== undefined){
-              data7 = await updateRepo(repoID, projID, "JakobMcKenna", "NoNameExists");
-          }
-          expect(data7).toBeNull();
-        });
 
-
+  test('Delete all info', async () => {
     let data7 = null
-    if(projID !== undefined){
-        data7 = await removeProject(projID);
+    if(projID1 !== undefined){
+        data7 = await removeProject(projID1);
     }
-    if(projID !== undefined){
-      data7 = await getProject(projID);
+    if(projID1 !== undefined){
+      data7 = await getProject(projID1);
   }
     expect(data7?.name).toBe(undefined);
 
     let data2 = null
-    if(userID !== undefined){
-        data2 = await deleteUserByID(userID);
+    if(userID1 !== undefined){
+        data2 = await deleteUserByID(userID1);
       }
       expect(data2).toBeNull();
   });
