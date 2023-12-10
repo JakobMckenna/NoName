8f704b6730f546c9cea0611454fc8d2d3b8f2c14
Tehilah <tehillahk@outlook.com>
2023-12-10 17:29:49 -0600

    removed anything that is unused

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -25,6 +25,7 @@ function LoadingTile() {
     )
 }
 
+
 function Tile({ id, name, user, parent }: { id: string, name: string, user: string, parent: any }) {
     return (
         <Link ref={parent} href={`/project/${id}`} className="card w-96 bg-primary glass text-primary-content shadow-xlw-full mb-10 hover:-translate-y-3 hover:-skew-y-3 duration-75">
@@ -32,14 +33,11 @@ function Tile({ id, name, user, parent }: { id: string, name: string, user: stri
                 <h2 className="card-title capitalize">{name} </h2>
                 <p> Created by {user} </p>
             </div>
-
         </Link>
     )
 }
 
 function ProjectHero({ projects, parent, search, projectsInit }: { projects: any, parent: Ref, search: any, projectsInit: any }) {
-    // console.log(projects)
-
     return (
         <div className="hero  bg-base-100">
             <div className="hero-content ">
@@ -98,8 +96,6 @@ function ProjectHero({ projects, parent, search, projectsInit }: { projects: any
                             </>
                         )
 
-
-
                     }
 
                 </div>
@@ -110,16 +106,12 @@ function ProjectHero({ projects, parent, search, projectsInit }: { projects: any
 
 
 export default function LandingPage() {
-    const router = useRouter();
-    const [user, loading] = useUser()
-    const [projectList, setProjectList] = useState<any[] | null>(null)
-    
-    const [currentTheme, loadingTheme] = useCurrentTheme()
-    const [theme, setTheme] = useState<string>()
-    const [parent, enableAnimations] = useAutoAnimate(/* optional config */)
-    const [filteredList, setFilteredList] = useState<any[]>([])
+    const [user, loading] = useUser();
+    const [projectList, setProjectList] = useState<any[] | null>(null);
+    const [parent, enableAnimations] = useAutoAnimate();
+    const [filteredList, setFilteredList] = useState<any[]>([]);
 
-
+    enableAnimations(true);
 
     const addProject = (project: any) => {
         setFilteredList((prev) => [project, ...prev]);
@@ -137,37 +129,28 @@ export default function LandingPage() {
     // filter projects by project name
     const searchProjects = (projectName: string) => {
         const results = projectList?.filter((project) => {
-            return project.project.name.toLowerCase().includes(projectName.toLocaleLowerCase())
-        })
+            return project.project.name.toLowerCase().includes(projectName.toLocaleLowerCase());
+        });
         if (results)
-            setFilteredList(results)
+            setFilteredList(results);
     }
 
     useEffect(
         () => {
-
+           
             //only get data when user data is loaded
-            if (user) {
+            if (user && !loading && !projectList && !Array.isArray(projectList) ) {
                 const projects = async () => {
                     const results = await getProjects(user?.id)
                     setProjectList(results?.member);
                     setFilteredList(results?.member)
                     return results.project;
                 }
-
-
-                if (!projectList && !Array.isArray(projectList) ) {
-                    projects();
-                }
-                //set current theme
-                if (currentTheme != null) {
-                    setTheme(currentTheme as unknown as string)
-                }
-
+                
+                projects();
             }
 
-
-        }, [user,theme, projectList]
+        }, [user, projectList]
     )
     return (
         <Drawer userName={user!=null && (user.name!=undefined || user.name!=null)?`${user.name}#${user.id}`:""}>
